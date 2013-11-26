4e370c92d842f6f936043d1fb61b83abe4ca7560
Joe <laymanje@gmail.com>
2013-11-26 14:13:50 -0500

    a couple of them

--- app/arrays.js
+++ app/arrays.js
@@ -3,14 +3,40 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+        return arr.indexOf(item);
     },
 
     sum : function(arr) {
+        
+        var total = 0;
 
+        for ( var i = 0 ; i < arr.length ; i++ ){
+            total += arr[i];
+        }
+
+        return total;
     },
 
     remove : function(arr, item) {
+
+        console.log(arr);
+
+        var 
+            newArray = [],
+            location = arr.indexOf(item)
+        ;
+
+        console.log(location)
+
+        for ( var i = 0 ; i < arr.length ; i++ ){
+            if( i != location ) {
+                console.log(i + ' : '+ location);
+                console.log();
+                newArray.push( arr );
+            }
+        }
+
+        return newArray;
 
     },
 

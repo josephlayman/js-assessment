efad9f9a85479d42fa5c31a7aa3eba27ee7ab263
Joe <Joe@Joes-MacBook-Pro.local>
2014-02-18 22:40:47 -0500

    async

--- app/async.js
+++ app/async.js
@@ -3,10 +3,26 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+    	var deferred = $.Deferred();
 
+      	setTimeout(function() {
+        	deferred.resolve(value);
+      	}, 10);
+      	
+      	return deferred.promise();
     },
 
     manipulateRemoteData : function(url) {
+    	var deferred = $.Deferred();
+
+	    $.getJSON(url).then(function(response) {
+	    	var people = $.map(response.people, function(person) {
+	    		return person.name;
+	    	});
+	    	deferred.resolve(people.sort());
+	    });
+
+	    return deferred.promise();
 
     }
   };
